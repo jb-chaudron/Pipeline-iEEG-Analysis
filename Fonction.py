ffbf790cead5c31575eec2a07b3ea1e4c3a2bce4
jb-chaudron <jb.chaudron8@gmail.com>
2021-03-27 17:30:42 +0100

    Update Fonction.py

--- Fonction.py
+++ Fonction.py
@@ -1,20 +1,25 @@
 
 class iEEG(object):
 	"""docstring for iEEG"""
-	def __init__(self, label, classes, epoch=None):
+	def __init__(self, label, y, epoch=None):
 		super(iEEG, self).__init__()
 		self.epoch = epoch
-		self.label = label
-		self.classes = classes
+		self.label = pd.DataFrame()
+		self.y = y
+		self.X = np.array()
 
 	def epoch(self, epoch):
 		self.epoch = epoch
 
-	def X(self, X):
-		self.X = X
-
 	def hilb(self,lfr,hfr):
-		return self.epoch.copy().filter(l_freq=f,h_freq=f+10,n_jobs=-1,fir_design='firwin',picks='all').apply_hilbert(n_jobs=-1,envelope=True)
+		"""
+			Fonction pour appliquer Hilbert, à voir si on veut utiliser d'autres paramètres plus tard
+
+			1 - Applique un filtre entre f et f+10
+			2 - Calcul l'enveloppe du signal
+			3 - Récupère les données
+		"""
+		return self.epoch.copy().filter(l_freq=f,h_freq=f+10,n_jobs=-1,fir_design='firwin',picks='all').apply_hilbert(n_jobs=-1,envelope=True).get_data()
 
 	def get_X(self, plage=[(a,a+10) for a in range(10,120,10)], f_bin=10, tranformation="Hilbert"):
 		"""
@@ -33,7 +38,7 @@ class iEEG(object):
 
 				Shape epoch.get_data() : m_epochs, n_contacts, o_millisecondes
 			"""
-			out = np.concatenate([sts.zscore(hilb(f,f+f_bin).get_data(),axis=2) for f in range(lfr,hfr,f_bin)],axis=2).mean(axis=2)
+			out = np.concatenate([sts.zscore(hilb(f,f+f_bin),axis=2) for f in range(lfr,hfr,f_bin)],axis=2).mean(axis=2)
 			ep_out = mne.EpochsArray(out,
 									info= mne.create_info( ch_names=self.epoch.ch_names,
 														   ch_types=['eeg' for x in range(len(self.epoch.ch_names))],
@@ -41,6 +46,7 @@ class iEEG(object):
 									tmin=self.epoch.tmin)
 
 			del out
+
 			"""
 				Baseline Normalisation
 
@@ -55,7 +61,8 @@ class iEEG(object):
 			X_ep = ep_out.copy().crop(tmin,tmax).get_data()
 
 			binnage = lambda a,b : np.apply_along_axis(lambda c: sts.binned_statistic(range(len(c)),c,bins=b)[0],axis=2,arr=a)
-
+			self.label["freq"].append()  
+			
 			if X_bin !=0 : 
 				# Créer N morceaux du signaux, qui sont moyennés
 				X = np.nan_to_num(binnage(X_ep,X_bin) - binnage(X_base,X_bin),nan=0.0,posinf=0.0,neginf=0.0)
@@ -67,14 +74,19 @@ class iEEG(object):
 
     		del X
 
+    	self.X = np.array(stacked_X)
 
 
 
-		def X_y(ep_hilb,tmin,tmax,cond=0,bin=1):
-  if cond == 0:
-    X = get_X(ep_hilb,tmin,tmax,bin=bin)
-    y = np.array(ep_hilb.metadata.loc[:,"error"]) 
-  else:
-    X = get_X(ep_hilb[cond],tmin,tmax,bin=bin)
-    y = np.array(ep_hilb[cond].metadata.loc[:,"error"]) 
-  return X,y
+    def Feature_reduction(self,bloc,taille=400):
+    	if len(bloc) == 0 :
+    		bloc = bloc[0]
+    		X_true = X[bloc]
+  		else:
+    		X_true = np.concatenate([X[b] for b in bloc])
+    		y = np.concatenate([Y[b] for b in bloc])
+    		
+  		feat = SelectKBest(mutual_info_classif, k=taille).fit(X_true, y).get_support()
+  		
+  		self.X = X_true[:,feat]
+		self.label = self.label[feat]
